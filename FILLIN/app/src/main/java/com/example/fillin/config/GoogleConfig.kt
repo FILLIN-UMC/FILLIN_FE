0f35d4d8f502ece597a1ac0d339d6f03c574863a
SON <jhon0129@naver.com>
2026-01-09 12:40:33 +0900

    Login File

--- FILLIN/app/src/main/java/com/example/fillin/config/GoogleConfig.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fillin.config
-
-object GoogleConfig {
-    const val WEB_CLIENT_ID =
-        "222936304688-h45k5mla4um79cdoqs0pi4huansb6nfu.apps.googleusercontent.com"
-}
--- FILLIN/app/src/main/java/com/example/fillin/ui/navigation.kt
+++
@@ -1,2 +0,0 @@
-package com.example.fillin.ui.theme
-
