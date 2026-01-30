579114a4fd971a57aec9a020ddf277e4fa8ccbdf
SON <80566363+Icanndoit@users.noreply.github.com>
2026-01-30 18:51:16 +0900

    Update SearchViewModelFactory.kt

--- app/src/main/java/com/example/fillin/feature/search/SearchViewModelFactory.kt
+++ app/src/main/java/com/example/fillin/feature/search/SearchViewModelFactory.kt
@@ -1,41 +1,4 @@
-//package com.example.fillin.ui.search
-//
-//import android.content.Context
-//import androidx.lifecycle.ViewModel
-//import androidx.lifecycle.ViewModelProvider
-//import com.example.fillin.data.remote.kakao.KakaoRetrofit
-//import com.example.fillin.data.repository.FakeHotReportRepository
-//import com.example.fillin.data.repository.HotReportRepository
-//import com.example.fillin.data.repository.KakaoPlaceRepository
-//
-//class SearchViewModelFactory(
-//    private val context: Context,
-//    // ✅ (3) 위치 기반 HOT 실데이터(Fake) 주입: 반경/개수 튜닝 가능
-//    private val hotRepo: HotReportRepository = FakeHotReportRepository(
-//        radiusMeters = 2000,
-//        maxItems = 20
-//    )
-//) : ViewModelProvider.Factory {
-//
-//    @Suppress("UNCHECKED_CAST")
-//    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-//        // ✅ PlaceRepo (Kakao)
-//        val placeRepo = KakaoPlaceRepository(KakaoRetrofit.api)
-//
-//        // ✅ ViewModel 타입 체크(안전)
-//        if (modelClass.isAssignableFrom(SearchViewModel::class.java)) {
-//            return SearchViewModel(
-//                context,
-//                placeRepo,
-//                hotRepo
-//            ) as T
-//        }
-//
-//        throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
-//    }
-//}
-
-package com.example.fillin.ui.search
+package com.example.fillin.feature.search
 
 import android.content.Context
 import androidx.lifecycle.ViewModel
