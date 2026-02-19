1cf952cf8a1679933caed4a2d9db509a688e59f1
answjddn0607 <166084634+answjddn0607@users.noreply.github.com>
2026-02-19 11:47:14 +0900

    Refactor: 수정 Splash 화면 디자인 변경

--- app/src/main/java/com/example/fillin/ui/components/FillinBackground.kt
+++ app/src/main/java/com/example/fillin/ui/components/FillinBackground.kt
@@ -1,27 +1,26 @@
 package com.example.fillin.ui.components
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.example.fillin.R
 
 @Composable
 fun FillinBlueGradientBackground(content: @Composable () -> Unit) {
-    val bg = Brush.radialGradient(
-        colors = listOf(
-            Color(0xFFBFE7FF), // 가장자리 밝은 하늘색
-            Color(0xFF4FA6E6)  // 중앙쪽 진한 파랑
-        )
-    )
-
     Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(bg)
+        modifier = Modifier.fillMaxSize()
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.img_background),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillBounds
+        )
+
         content()
     }
 }
